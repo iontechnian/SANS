ceeac22772b132f7c41bb1a4d2475d9e973ef681
Iontechnian <ilan.f.garcia@gmail.com>
2018-06-23 03:02:56 -0300

    salien script injection command

---
+++ script.js
@@ -0,0 +1,5 @@
+var head = document.getElementsByTagName('head').item(0);
+var script = document.createElement('script');
+script.setAttribute('type', 'text/javascript');
+script.setAttribute('src', 'https://raw.githubusercontent.com/iontechnian/SANS/master/dist/salien.js');
+head.appendChild(script);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
+    entry: {
+        salien: './index.js',
+        script: './script.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'salien.js'
+        filename: '[name].js'
     }
 }
